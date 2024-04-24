f092062f0c4a54836f6690028ccd90854d691c7f
cedricdcc <30471340+cedricdcc@users.noreply.github.com>
2024-04-24 18:23:08 +0200

    Refactor entrypoint.sh and update release_management.yml workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -49,6 +49,8 @@ python ymls_to_ldes_ttl.py
 # usr folder
 # var folder
 
+cd ..
+
 rsync --recursive --progress -avzh --exclude=.git --exclude=.github --exclude=.dockerenv --exclude=README.md --exclude=bin --exclude=boot --exclude=config.yml --exclude=dev --exclude=entrypoint.sh --exclude=etc --exclude=github --exclude=home --exclude=lib --exclude=lib64 --exclude=media --exclude=mnt --exclude=opt --exclude=proc --exclude=node_modules --exclude=package-lock.json --exclude=package.json --exclude=poetry.lock --exclude=pyproject.toml --exclude=run --exclude=root --exclude=sbin --exclude=src --exclude=srv --exclude=sys --exclude=tmp --exclude=usr --exclude=var ./ ./github/workspace
 
 rsync --recursive --progress -avzh ./.github/last_ldes_hash ./github/workspace/.github/last_ldes_hash
