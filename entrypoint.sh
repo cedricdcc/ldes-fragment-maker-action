69e6f2473ef117f8b57b0f89b98d1e063ec6b478
cedricdcc <30471340+cedricdcc@users.noreply.github.com>
2024-04-24 12:00:47 +0200

    Refactor entrypoint.sh and remove setup.sh script

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,6 @@ pwd
 
 ls -al
 
-echo "repo name is:" GITHUB_REPOSITORY
-
 cd ../..
 pwd
 ls -al
@@ -14,25 +12,43 @@ rsync --recursive --progress -avzh ./github/workspace/* ./
 cd src
 
 # executing the setup.sh script
-bash setup.sh
+python ymls_to_ldes_ttl.py
 
 # copy over everything in ./ back to ./github/workspace
 # overwrite everything in ./github/workspace
 # do not copy over the .git folder and the .github folder 
 # also do not copy over the following files:
-# - entrypoint.sh
-# - Dockerfile
-# - README.md
-# - LICENSE
-# - .gitignore
-# - .dockerignore
-# - .env
+# .dockerenv
+# README.md
+# bin folder
+# boot folder
+# config.yml
+# dev folder
+# entrypoint.sh
+# etc folder
+# github folder
+# home folder
+# lib folder
+# lib64 folder
+# media folder
+# mnt folder
+# opt folder
+# proc folder
+# node_modules folder
+# package-lock.json
 # package.json
 # poetry.lock
 # pyproject.toml
-# requirements.txt
-# everything in src folder and recursively under src folder
-# package-lock.json
-# actions.yml
+# run folder
+# root folder
+# sbin folder
+# src folder
+# srv folder
+# sys folder
+# tmp folder
+# usr folder
+# var folder
 
-rsync --recursive --progress -avzh --exclude='.git' --exclude='.github' --exclude='entrypoint.sh' --exclude='Dockerfile' --exclude='README.md' --exclude='LICENSE' --exclude='.gitignore' --exclude='.dockerignore' --exclude='.env' --exclude='package.json' --exclude='poetry.lock' --exclude='pyproject.toml' --exclude='requirements.txt' --exclude='src' --exclude='package-lock.json' --exclude='action.yml' ./ ./github/workspace/
+rsync --recursive --progress -avzh --exclude=.git --exclude=.github --exclude=.dockerenv --exclude=README.md --exclude=bin --exclude=boot --exclude=config.yml --exclude=dev --exclude=entrypoint.sh --exclude=etc --exclude=github --exclude=home --exclude=lib --exclude=lib64 --exclude=media --exclude=mnt --exclude=opt --exclude=proc --exclude=node_modules --exclude=package-lock.json --exclude=package.json --exclude=poetry.lock --exclude=pyproject.toml --exclude=run --exclude=root --exclude=sbin --exclude=src --exclude=srv --exclude=sys --exclude=tmp --exclude=usr --exclude=var ./ ./github/workspace
+
+rsync --recursive --progress -avzh ./.github/last_ldes_hash ./github/workspace/.github/last_ldes_hash
